1b9b8d1c9a5c67e8c0439cd8afd8811b1a890703
impureblack <impureblack@gmail.com>
2016-11-28 09:02:06 +0000

    updated: ページタイトル生成をメソッド化

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    
+    #
+    #ページタイトルを出力します
+    #
+    def fullTitle(pageTitle)
+        baseTitle = "Ruby on Rails Tutorial Sample App"
+        if pageTitle.empty?
+            baseTitle
+        else
+            "#{baseTitle} | #{pageTitle}"
+        end
+    end
+    
 end

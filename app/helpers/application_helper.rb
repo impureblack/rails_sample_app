36191c8a2fd90ee0db8045b09b4f4e02233fa8af
impureblack <impureblack@gmail.com>
2016-12-09 21:03:53 +0000

    added: #1 deviseをインストール

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,36 @@
 module ApplicationHelper
     
     #
-    #ページタイトルを出力します
+    # ページタイトルを出力します。
+    # @params page_title
+    # @return ベースタイトル＋引数のタイトル
     #
-    def fullTitle(pageTitle)
-        baseTitle = "Ruby on Rails Tutorial Sample App"
-        if pageTitle.empty?
-            baseTitle
+    def full_title(page_title)
+        base_title = "Ruby on Rails Tutorial Sample App"
+        if page_title.empty?
+            base_title
         else
-            "#{baseTitle} | #{pageTitle}"
+            "#{base_title} | #{page_title}"
+        end
+    end
+    
+    #
+    # flash変数をbootstrapクラスに変換します。
+    # @params flash_title
+    # @return bootstrapクラス
+    #
+    def bootstrap_class_for(flash_type)
+        case flash_type
+            when :success
+                "alert_success"
+            when :error
+                "alert_error"
+            when :alert
+                "alert_warning"
+            when :notice
+                "alert_notice"
+            else
+                flash_type.to_s
         end
     end
     
